e0f94624fd14b0db9dbcddf438d3547822335576
Deva658 <devaaditya271@gmail.com>
2025-05-17 14:04:59 +0700

    Menambahkan class Buku untuk menyimpan data buku

--- Constructor.cpp
+++ Constructor.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Petugas;
+class Admin;
+
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void infoBuku() const {
+        cout << "Judul: " << judul << ", Penulis: " << penulis 
+             << ", Status: " << (dipinjam ? "Dipinjam" : "Tersedia") << endl;
+    }
+
+    friend class Petugas;
+    friend void aksesTerbatas(Admin* a, Buku* b); // hanya untuk melihat info
+};
+
