e85a13a993be13caf0aa1b2863baefe52f2f98fa
Deva658 <devaaditya271@gmail.com>
2025-05-17 14:06:04 +0700

    Membuat class Peminjam yang menyimpan nama, ID, dan total buku yang dipinjam

--- Constructor.cpp
+++ Constructor.cpp
@@ -23,3 +23,21 @@ public:
     friend void aksesTerbatas(Admin* a, Buku* b); // hanya untuk melihat info
 };
 
+class Peminjam {
+private:
+    string nama;
+    int id;
+    int totalPinjaman;
+
+public:
+    Peminjam(string n, int i) : nama(n), id(i), totalPinjaman(0) {}
+
+    void infoPeminjam() const {
+        cout << "Nama: " << nama << ", ID: " << id 
+             << ", Total Pinjaman: " << totalPinjaman << endl;
+    }
+
+    friend class Petugas;
+    friend void aksesTerbatas(Admin* a, Peminjam* p); // hanya untuk melihat info
+};
+
